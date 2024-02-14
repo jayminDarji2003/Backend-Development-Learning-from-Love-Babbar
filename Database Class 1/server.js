ed4350c64fbca172ed5c9d9a1cf6bcfaf6716f23
Jaymin Darji <jaymindarji2003@gmail.com>
2024-02-14 11:45:11 +0530

    half class 1 done

---
+++ Database Class 1/server.js
@@ -0,0 +1,31 @@
+// importing the express
+const express = require('express');
+
+// create the instance of express
+const app = express();
+
+// create the port
+const PORT = 3000;
+
+// listening the server
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+})
+
+// body parser to get the data from body
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());  // parsing the json data and add it to request body object
+
+// creating our first route "/"
+// get request
+app.get('/', (req, res) => {
+    res.send("<h1>Welcome to my first route!</h1>");
+});
+
+// creating post request
+app.post('/api/cars', (req, res) => {
+    const { name, brand } = req.body;
+    console.log(name)
+    console.log(brand)
+    res.send("car submitted successfully")
+})
