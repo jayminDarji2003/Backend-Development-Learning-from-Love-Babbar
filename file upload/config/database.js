ef14067ea395ff08747138f0b8f6df71d86f04c6
Jaymin Darji <jaymindarji2003@gmail.com>
2024-05-28 12:12:24 +0530

    start project

---
+++ file upload/config/database.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");  // importing mongoose
+require("dotenv").config()  // importing .env file data
+
+const dbConnect = async () => {
+    try {
+        const connect = await mongoose.connect(process.env.DATABASE_URL);
+        console.log("Connected to database successfully");
+    } catch (err) {
+        console.log(err);
+        console.log("Error occurred while connecting to MongoDB");
+        process.exit(1);
+    }
+}
+
+module.exports = dbConnect;
---
+++ file upload/readme.md
@@ -0,0 +1,5 @@
+# File Uploader App
+
+- In this project, we are going to make a app that will upload files like Images, Video to cloud storage
+
+
